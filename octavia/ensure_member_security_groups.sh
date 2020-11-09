5fbbdd21bdc6301341fc3ae1542ee0075d9a0da8
Edward Hope-Morley <edward.hope-morley@canonical.com>
2020-11-09 16:19:33 +0000

    [octavia] improve error reporting

--- octavia/ensure_member_security_groups.sh
+++ octavia/ensure_member_security_groups.sh
@@ -4,6 +4,7 @@
 # group rule containing a rule that opens the port being loadbalanced.
 #
 SCRATCH_AREA=`mktemp -d`
+LOADBALANCER=${1:-}
 
 cleanup ()
 {
@@ -17,21 +18,43 @@ if [ -z "${OS_AUTH_URL:-}" ]; then
     source $openrc_path
 fi
 
-echo "INFO: pre-fetching information"
 mkdir -p $SCRATCH_AREA/results
+
+echo -n "INFO: pre-fetching information..."
+
+# Get all ports
+echo -n "[ports]"
 openstack port list -c ID -c fixed_ips -f value > $SCRATCH_AREA/port_list &
-openstack loadbalancer list -c id -f value > $SCRATCH_AREA/loadbalancer_list &
+
+# Get all LBs and allow single loadbalancer override
+echo -n "[loadbalancers]"
+if [ -n "$LOADBALANCER" ]; then
+    echo "$LOADBALANCER" > $SCRATCH_AREA/loadbalancer_list
+else
+    openstack loadbalancer list -c id -f value > $SCRATCH_AREA/loadbalancer_list &
+fi
 wait
-# get pools
+
+# Extract port info
+while read -r port_info; do
+    uuid=${port_info%% *}
+    mkdir -p $SCRATCH_AREA/ports/$uuid
+    # note: format of this field changes across releases so this may need updating
+    address=`echo ${port_info#* }| \
+                 sed -rn -e "s/.+ip_address='([[:digit:]\.]+)',\s+.+/\1/" \
+                         -e "s/.+ip_address':\s+'([[:digit:]\.]+)'}.+/\1/p"`
+    echo $address > $SCRATCH_AREA/ports/$uuid/address
+done < $SCRATCH_AREA/port_list
+
+# Get pools, listeners and members
+echo -n "[pools+members+listeners]"
 while read -r lb; do
     mkdir -p $SCRATCH_AREA/$lb/pools
     for pool in `openstack loadbalancer pool list -c id -f value --loadbalancer $lb`; do
         mkdir -p $SCRATCH_AREA/$lb/pools/$pool
     done &
-done < $SCRATCH_AREA/loadbalancer_list
-wait
-# get listeners and members
-while read -r lb; do
+    wait
+
     for pool in `ls $SCRATCH_AREA/$lb/pools`; do
         mkdir -p $SCRATCH_AREA/$lb/listeners
         for listener in `openstack loadbalancer listener list| awk "\\$4==\"$pool\" {print \\$2}"`; do
@@ -49,25 +72,17 @@ while read -r lb; do
 done < $SCRATCH_AREA/loadbalancer_list
 wait
 
-# Get detailed port info
-while read -r port_info; do
-    uuid=${port_info%% *}
-    mkdir -p $SCRATCH_AREA/ports/$uuid
-    # note: format of this field changes across releases so this may need updating
-    address=`echo ${port_info#* }| \
-                 sed -rn -e "s/.+ip_address='([[:digit:]\.]+)',\s+.+/\1/" \
-                         -e "s/.+ip_address':\s+'([[:digit:]\.]+)'}.+/\1/p"`
-    echo $address > $SCRATCH_AREA/ports/$uuid/address
-done < $SCRATCH_AREA/port_list
+echo ""
 
 # Run checks
-echo "INFO: checking loadbalancers '`cat $SCRATCH_AREA/loadbalancer_list| tr -s '\n' ' '`'"
+echo "INFO: checking loadbalancers '`cat $SCRATCH_AREA/loadbalancer_list| tr -s '\n' ' '| sed -r 's/\s+$//g'`'"
 while read -r lb; do
     (
     mkdir -p $SCRATCH_AREA/results/$lb
     for pool in `ls $SCRATCH_AREA/$lb/pools`; do
         for listener in `ls $SCRATCH_AREA/$lb/listeners`; do
             listener_port=`cat $SCRATCH_AREA/$lb/listeners/$listener/port`
+            error_idx=0
             for member in `ls $SCRATCH_AREA/$lb/pools/$pool/members`; do
                 address=`cat $SCRATCH_AREA/$lb/pools/$pool/members/$member/address`
                 path=`egrep -lr "$address$" $SCRATCH_AREA/ports`
@@ -82,7 +97,13 @@ while read -r lb; do
                         found=true
                     done < $SCRATCH_AREA/sgrules/$sg
                     if ! $found; then
-                        echo "$listener_port" >> $SCRATCH_AREA/results/$lb/missing_rules_ports
+                        error_path=$SCRATCH_AREA/results/$lb/errors/$error_idx
+                        mkdir -p $error_path
+                        echo "$listener_port" > $error_path/protocol_port
+                        echo $member > $error_path/member
+                        echo $port > $error_path/backend_vm_port
+                        echo $sg > $error_path/security_group
+                        ((error_idx+=1))
                     fi
                 done
             done
@@ -92,12 +113,16 @@ while read -r lb; do
 done < $SCRATCH_AREA/loadbalancer_list
 wait
 
-ls $SCRATCH_AREA/results/*/missing_rules_ports &>/dev/null && echo ""
-for missing in `find $SCRATCH_AREA/results/ -name missing_rules_ports`; do
-    lb=`dirname $missing`
-    lb=`basename $lb`
-    echo "WARNING: loadbalancer $lb has member(s) with security groups that don't have required ports open: `cat $missing`"
+ls $SCRATCH_AREA/results/*/errors &>/dev/null && echo ""
+for errors in `find $SCRATCH_AREA/results -name errors`; do
+    lb=$(basename `dirname $errors`)
+    for error in `ls $errors`; do
+        port=`cat $errors/$error/protocol_port`
+        echo "WARNING: loadbalancer $lb has member(s) with security groups that don't have required ports open: $port"
+        echo "Details:"
+        ( cd $errors/$error; grep . *; )
+    done
 done
-ls $SCRATCH_AREA/results/*/missing_rules_ports &>/dev/null && echo ""
+ls $SCRATCH_AREA/results/*/errors &>/dev/null && echo ""
 
 echo "Done."
