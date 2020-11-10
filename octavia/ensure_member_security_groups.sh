09e8e90f69cf7f160d38a8d415080d412d121f9e
Edward Hope-Morley <edward.hope-morley@canonical.com>
2020-11-10 14:36:40 +0000

    [octavia] add more checks and snap support

--- octavia/ensure_member_security_groups.sh
+++ octavia/ensure_member_security_groups.sh
@@ -40,10 +40,15 @@ while read -r port_info; do
     uuid=${port_info%% *}
     mkdir -p $SCRATCH_AREA/ports/$uuid
     # note: format of this field changes across releases so this may need updating
-    address=`echo ${port_info#* }| \
-                 sed -rn -e "s/.+ip_address='([[:digit:]\.]+)',\s+.+/\1/" \
-                         -e "s/.+ip_address':\s+'([[:digit:]\.]+)'}.+/\1/p"`
-    echo $address > $SCRATCH_AREA/ports/$uuid/address
+    ip_address=`echo ${port_info#* }| \
+                  sed -rn -e "s/.+ip_address='([[:digit:]\.]+)',\s+.+/\1/" \
+                          -e "s/.+ip_address':\s+'([[:digit:]\.]+)'}.+/\1/p"`
+    subnet_id=`echo ${port_info#* }| \
+                 sed -rn -e "s/.+subnet_id='([[:alnum:]\-]+)',.+/\1/" \
+                         -e "s/.+\{'subnet_id':\s+'([[:alnum:]\-]+)',.+/\1/p"`
+    #[ -n "$ip_address" ] && [ -n "$subnet_id" ] || echo "WARNING: incomplete information for port $uuid"
+    echo $ip_address > $SCRATCH_AREA/ports/$uuid/ip_address
+    echo $subnet_id > $SCRATCH_AREA/ports/$uuid/subnet_id
 done < $SCRATCH_AREA/port_list
 
 # Get pools, listeners and members
@@ -65,7 +70,9 @@ while read -r lb; do
         done &
         for member in `openstack loadbalancer member list -c id -f value $pool`; do
             mkdir -p $SCRATCH_AREA/$lb/pools/$pool/members/$member
-            openstack loadbalancer member show -c address -f value $pool $member > $SCRATCH_AREA/$lb/pools/$pool/members/$member/address
+            readarray -t member_info<<<"`openstack loadbalancer member show -c address -c subnet_id -f value $pool $member`"
+            echo "${member_info[0]}" > $SCRATCH_AREA/$lb/pools/$pool/members/$member/address
+            echo "${member_info[1]}" > $SCRATCH_AREA/$lb/pools/$pool/members/$member/subnet_id
         done &
         wait
     done &
@@ -74,14 +81,47 @@ wait
 
 echo ""
 
-get_security_group ()
+fetch_security_group_rules ()
 {
+    # get sg rules if they are not already cached
     local sg=$1
-    mkdir -p $SCRATCH_AREA/security_groups/$sg/rules
+    local sg_path=$SCRATCH_AREA/security_groups/$sg/rules/
+
+    ! [ -d $sg_path ] || return
+    mkdir -p $sg_path
     for rule in `openstack security group rule list $sg -c ID -f value`; do
-        openstack security group rule show $rule > $SCRATCH_AREA/security_groups/$sg/rules/$rule &
+        openstack security group rule show $rule > $sg_path/$rule &
     done
     wait
+}
+
+get_subnet_cidr ()
+{
+    local subnet=$1
+    if ! [ -r "$SCRATCH_AREA/subnets/$subnet/cidr" ]; then
+        mkdir -p $SCRATCH_AREA/subnets/$subnet/
+        openstack subnet show $subnet -c cidr -f value > $SCRATCH_AREA/subnets/$subnet/cidr
+    fi
+    cat $SCRATCH_AREA/subnets/$subnet/cidr
+    return
+}
+
+get_member_port_uuid ()
+{
+    local m_subnet_id=$1
+    local m_address=$2
+    local port=
+    local port_ip_address=
+
+    readarray -t ports<<<`find $SCRATCH_AREA/ports -name subnet_id| xargs -l egrep -l "$m_subnet_id$"`
+    for path in ${ports[@]}; do
+        [ -n "$path" ] || continue
+        port=`dirname $path`
+        port_ip_address=`cat $port/ip_address`
+        [[ "$port_ip_address" == "$m_address" ]] || continue
+        basename $port
+        return 0
+    done
 }
 
 # Run checks
@@ -94,29 +134,43 @@ while read -r lb; do
         for listener in `ls $SCRATCH_AREA/$lb/listeners`; do
             listener_port=`cat $SCRATCH_AREA/$lb/listeners/$listener/port`
             error_idx=0
-            for member in `ls $SCRATCH_AREA/$lb/pools/$pool/members`; do
-                address=`cat $SCRATCH_AREA/$lb/pools/$pool/members/$member/address`
-                path=`egrep -lr "$address$" $SCRATCH_AREA/ports`
-                port=$(basename `dirname $path`)
-                for sg in `openstack port show -c security_group_ids -f value $port| egrep -o "[[:alnum:]\-]+"`; do
-                    sg_path=$SCRATCH_AREA/security_groups/$sg
-                    [ -d $sg_path ] || get_security_group $sg
+            for member_uuid in `ls $SCRATCH_AREA/$lb/pools/$pool/members`; do
+                m_address=`cat $SCRATCH_AREA/$lb/pools/$pool/members/$member_uuid/address`
+                m_subnet_id=`cat $SCRATCH_AREA/$lb/pools/$pool/members/$member_uuid/subnet_id`
+
+                # find port with this address and subnet_id
+                port_uuid=`get_member_port_uuid $m_subnet_id $m_address`
+                if [[ -z "$port_uuid" ]]; then
+                    echo "WARNING: unable to identify member port for address=$m_address, subnet=$m_subnet_id - skipping member $member_uuid"
+                    continue
+                fi
+
+                for sg in `openstack port show -c security_group_ids -f value $port_uuid| egrep -o "[[:alnum:]\-]+"`; do
+                    fetch_security_group_rules $sg
                     found=false
-                    for rule in `find $sg_path/rules/ -type f`; do
+                    for rule in `find $SCRATCH_AREA/security_groups/$sg/rules/ -type f`; do
                         # THIS IS THE ACTUAL CHECK - ADD MORE AS NEEDED #
-                        max_actual=`awk "\\$2==\"port_range_min\" {print \\$4}" $rule`
-                        min_actual=`awk "\\$2==\"port_range_max\" {print \\$4}" $rule`
-                        [[ "${min_actual}:${max_actual}" == "${listener_port}:${listener_port}" ]] || continue
+                        port_range_max=`awk "\\$2==\"port_range_min\" {print \\$4}" $rule`
+                        port_range_min=`awk "\\$2==\"port_range_max\" {print \\$4}" $rule`
+                        remote_ip_prefix=`awk "\\$2==\"remote_ip_prefix\" {print \\$4}" $rule`
                         direction=`awk "\\$2==\"direction\" {print \\$4}" $rule`
+
+                        # ensure port range
+                        [[ "${port_range_min}:${port_range_max}" == "${listener_port}:${listener_port}" ]] || continue
+                        # ensure ingress
                         [[ "$direction" == "ingress" ]] || continue
+                        # ensure correct network range
+                        [[ "$remote_ip_prefix" == "$(get_subnet_cidr `cat $subnet_id_path`)" ]] || continue
+
                         found=true
+                        break
                     done
                     if ! $found; then
                         error_path=$SCRATCH_AREA/results/$lb/errors/$error_idx
                         mkdir -p $error_path
                         echo "$listener_port" > $error_path/protocol_port
-                        echo $member > $error_path/member
-                        echo $port > $error_path/backend_vm_port
+                        echo $member_uuid > $error_path/member
+                        echo $port_uuid > $error_path/backend_vm_port
                         echo $sg > $error_path/security_group
                         for entry in `ls $error_path`; do
                             echo " - $entry: `cat $error_path/$entry`" >> $error_path/details
@@ -140,6 +194,10 @@ for errors in `find $SCRATCH_AREA/results -name errors`; do
         cat $errors/$error/details
     done
 done
-ls $SCRATCH_AREA/results/*/errors &>/dev/null && echo ""
+if `ls $SCRATCH_AREA/results/*/errors &>/dev/null`; then
+    echo ""
+else
+    echo "INFO: no issues found."
+fi
 
 echo "Done."
