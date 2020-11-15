6eb8e0897854cb2bab2a17828fbb60e465aebf2c
Edward Hope-Morley <edward.hope-morley@canonical.com>
2020-11-15 15:48:41 +0000

    [neutron] add l3agent state distribution check

--- neutron/run.sh
+++ neutron/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -u
-OPT_CHECK_ROUTER_L3HA_STATUS=true  # default to true since we have no other checks yet
+OPT_CHECK_ROUTER_L3HA_STATUS=false
 ARG_CHECK_ROUTER_L3HA_STATUS=
+OPT_CHECK_ROUTER_L3HA_STATE_DIST=false
 
 usage ()
 {
@@ -15,28 +16,40 @@ DESCRIPTION
 
         NOTE: this is currently the default.
 
+    --check-l3ha-router-state-distribution
+
+        Show tally of active/standby counts for HA routers per host/l3-agent.
+
 EOF
 }
 
+use_default=true
 while (($#)); do
     case "$1" in
         --check-l3ha-router-status)
-          OPT_CHECK_ROUTER_L3HA_STATUS=true
-          if (($#>1)) && [[ ${2:0:2} != "--" ]]; then
-              ARG_CHECK_ROUTER_L3HA_STATUS=$2
-              shift
-          fi
-          ;;
+              OPT_CHECK_ROUTER_L3HA_STATUS=true
+              if (($#>1)) && [[ ${2:0:2} != "--" ]]; then
+                  ARG_CHECK_ROUTER_L3HA_STATUS=$2
+                  shift
+              fi
+              use_default=false
+              ;;
+        --check-l3ha-router-state-distribution)
+              OPT_CHECK_ROUTER_L3HA_STATE_DIST=true
+              use_default=false
+              ;;
         --help|-h)
-          usage
-          exit 0
-          ;;
+              usage
+              exit 0
+              ;;
     esac
     shift
 done
 
-if $OPT_CHECK_ROUTER_L3HA_STATUS; then
+if $OPT_CHECK_ROUTER_L3HA_STATUS || $use_default; then
     `dirname $0`/plugins.d/check_router_l3agent_ha_status $ARG_CHECK_ROUTER_L3HA_STATUS
+elif $OPT_CHECK_ROUTER_L3HA_STATE_DIST; then
+    `dirname $0`/plugins.d/check_router_l3agent_ha_state_distribution
 else
     usage
 fi
